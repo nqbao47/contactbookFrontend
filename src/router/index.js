d7df04f1858b8764cd0c9384d95d73302bf34bf6
QuBao47 <baonguyengau14@gmai.com>
2022-10-28 15:52:24 +0700

    Implement the 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,15 @@ const routes = [
         name: 'contactbook',
         component: ContactBook,
     },
+
+    {
+        path: '/:pathMatch(.*)*',
+        name: 'notfound',
+        component: () => import('@/views/NotFound.vue'),
+        },
+        
 ];
+
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
